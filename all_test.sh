bba2dd35f8a26336b677c19c27248703c3e0a556
Yusuke <y38y38@gmail.com>
2021-08-26 23:08:56 +0900

    debug

--- all_test.sh
+++ all_test.sh
@@ -1,9 +1,11 @@
 rm -rf ./tmp
 mkdir -p ./tmp/
-for file_name in ../prores_decoder/test/input/*.yuv ;do
+for file_name in ../prores_decoder/test/input_sample10bit/*.yuv ;do
     OUT_NAME=`basename $file_name`
+    #../../prores_encoder/encoder -l ../../prores_encoder/luma_matrix.txt -c ../../prores_encoder/chroma_matrix.txt -q ../../prores_encoder/qscale_128x16.txt -h 128 -v 16 -m 8 -i ${file_name}  -o ./tmp/${OUT_NAME%.yuv}.bin  
     ./obj_dir/Vwrapper -l ./luma_matrix.txt -c ./chroma_matrix.txt -q ./qscale_128x16.txt -h 128 -v 16 -m 8 -i ${file_name}  -o ./tmp/${OUT_NAME%.yuv}.bin  
-    ../prores_decoder/decoder ./tmp/${OUT_NAME%.yuv}.bin ./tmp/${OUT_NAME%.yuv}_dec.yuv 
-    diff ./tmp/${OUT_NAME%.yuv}_dec.yuv ../prores_decoder//test/org/${OUT_NAME%.yuv}_dec.yuv
-    #../../yuv/sn16/sn16 ${file_name} ../prores_decoder/tmp/${OUT_NAME%.yuv}_dec.yuv 128 16
+#    ../../prores_decoder/decoder ./tmp/${OUT_NAME%.yuv}.bin ./tmp/${OUT_NAME%.yuv}_dec.yuv 
+#   diff ./tmp/${OUT_NAME%.yuv}_dec.yuv ./input_128x16/${OUT_NAME%.yuv}_dec.yuv
+    diff ./tmp/${OUT_NAME%.yuv}.bin ../prores_decoder/test/output_sample10bit/${OUT_NAME%.yuv}.bin
+    #../sn16/sn16 ${file_name} ./tmp/${OUT_NAME%.yuv}_dec.yuv 128 16
 done
